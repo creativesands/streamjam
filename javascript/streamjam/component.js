6288c4784a072446045d68aea1deeaedbf33b4b1
Sandeep S Kumar <sandeep@razorthink.com>
2024-01-03 09:49:46 -0800

    bugfix: remove message handlers on component destroy

--- javascript/streamjam/component.js
+++ javascript/streamjam/component.js
@@ -17,21 +17,21 @@ export class Component {
     }
 
     newStore(storeName, initialValue=null) {
-        const this_ = this
+        const self = this
         const { subscribe, set, update } = writable(initialValue)
 
         const topic = ['store-value', this.id, storeName].join('>')
         this.client.registerMessageHandler(topic, (value) => {
             set(value)
-            this.messageHandlerTopics.push(topic)
             console.debug('Setting new value for', storeName, value)
         })
+        this.messageHandlerTopics.push(topic)
 
         const store = {
             subscribe,
             set(value) {
                 update(_ => value)
-                this_.client.wsSend('store-set', [this_.id, storeName, value])
+                self.client.wsSend('store-set', [self.id, storeName, value])
             }
         }
         this.stores[storeName] = store
@@ -39,11 +39,11 @@ export class Component {
     }
 
     proxyRPC(rpcName) {
-        const this_ = this
+        const self = this
 
         const rpc = async function () {
-            const reqId = this_.client.wsSend('exec-rpc', [this_.id, rpcName, Array.from(arguments)])
-            let [_, content] = await this_.client.collectResponse(reqId)
+            const reqId = self.client.wsSend('exec-rpc', [self.id, rpcName, Array.from(arguments)])
+            let [_, content] = await self.client.collectResponse(reqId)
             return content
         }
 
@@ -52,7 +52,7 @@ export class Component {
     }
 
     destroy() {
-        this.messageHandlerTopics.forEach(this.client.removeMessageHandler)
+        this.messageHandlerTopics.forEach(this.client.removeMessageHandler.bind(this.client))
         this.client.wsSend('destroy-component', [this.id])
     }
 }
